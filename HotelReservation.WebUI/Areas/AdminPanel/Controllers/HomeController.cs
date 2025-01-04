8b810a63f9490902ebb126a40286c4f1c7fa919c
ÖĞRETMEN-PC <RETMEN-PC@LAB5-OGRETMEN>
2025-01-04 13:20:43 +0300

    .

---
+++ HotelReservation.WebUI/Areas/AdminPanel/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservation.WebUI.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class HomeController : Controller
+    {
+        [HttpGet("/Admin/Anasayfa")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
